16c33bc98da05e449ffd784188094576a9fd0964
moljski <moljski@gmail.com>
2022-02-24 14:56:11 +0100

    Componentization

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", complited: false },
+  { id: "todo-2", name: "Repeat", complited: false },
+];
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
